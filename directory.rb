896707cfb6ee97701322eaba93ee873ac27e2457
charlie casserly <charliecasserly215@gmailcom>
2021-04-14 20:59:50 +0000

    Added comments

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+#printing the title and list of students
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,7 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-
+#printing the number of students
 print "Overall, we have "
+#using print to interpolate the integer into the string
 print 11
 puts " great students"
