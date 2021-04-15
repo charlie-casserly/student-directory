4ae8a9e29dc3263fb976c092f450c406744029ff
charlie-casserly <charliecasserly215@gmail.com>
2021-04-15 20:25:35 +0000

    Refactored with interactive menu

--- directory.rb
+++ directory.rb
@@ -1,6 +1,6 @@
-def input_students
-  students = []
+@students = []
 
+def input_students
   while true do
     puts "Please enter the name of a student".center(50, '-')
     puts "To finish, just hit return twice".center(50, '-')
@@ -15,21 +15,19 @@ def input_students
     birthplace = gets.chomp.capitalize
     puts "Please enter #{name}'s height in cm".center(50, '-')
     height = gets.chomp
-    students << {name: name, cohort: cohort, hobby: hobby, birthplace: birthplace, height: height}
-    students.length == 1 ? (puts "Now we have 1 student") : (puts "Now we have #{students.count} students".center(50, '-'))
+    @students << {name: name, cohort: cohort, hobby: hobby, birthplace: birthplace, height: height}
+    @students.length == 1 ? (puts "Now we have 1 student") : (puts "Now we have #{@students.count} students".center(50, '-'))
   end
-  
-  students
 end
 
-def first_letter(students)
+def first_letter
   puts "View students beginning with which letter?".center(50, '-')
   puts "To finish, just hit return twice".center(50, '-')
   
   while true do
     letter = gets.chomp.capitalize
     break if letter.empty?
-    students.each do |student|
+    @students.each do |student|
       if student[:name].length < 12
         if student[:name][0] == letter
           puts "#{student[:name]} (#{student[:cohort]} cohort)"
@@ -45,27 +43,27 @@ def print_header
   puts "-------------".center(50, '-')
 end
 
-def print(students)
+def print_students_list
   counter = 0
-  while students.length > counter
-    if students[counter][:name].length < 12
-      puts "#{students[counter][:name]} (#{students[counter][:cohort]} cohort)"
+  while @students.length > counter
+    if @students[counter][:name].length < 12
+      puts "#{@students[counter][:name]} (#{@students[counter][:cohort]} cohort)"
     end
     counter += 1
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students".center(50, '-')
+def print_footer
+  puts "Overall, we have #{@students.count} great students".center(50, '-')
 end
 
-def print_by_cohort(students)
+def print_by_cohort
   puts "Which cohort would you like to print?".center(50, '-')
   puts "To finish, just hit return twice".center(50, '-')
   while true do
     input = gets.chomp.capitalize
     break if input.empty?
-    students.map do |student|
+    @students.map do |student|
       if student[:cohort] == input
         puts student[:name]
       end
@@ -74,15 +72,36 @@ def print_by_cohort(students)
   end
 end
 
-students = input_students
-while true do
-  if students.empty?
-    break
-  end
+def show_students
   print_header
-  print(students)
-  first_letter(students)
-  print_by_cohort(students)
-  print_footer(students)
-  break
+  print_students_list
+  print_footer
 end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      print_students_list
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+interactive_menu
