9196810edc90daa2b9912634bfb51151f0752f0a
charlie-casserly <charliecasserly215@gmail.com>
2021-04-16 15:33:22 +0000

    step 13 complete

--- directory.rb
+++ directory.rb
@@ -4,17 +4,17 @@ def input_students
   while true do
     puts "Please enter the name of a student".center(50, '-')
     puts "To finish, just hit return twice".center(50, '-')
-    name = gets.chomp.capitalize
+    name = STDIN.gets.chomp.capitalize
     break if name.empty?
     puts "Please enter #{name}'s cohort".center(50, '-')
-    cohort = gets.chomp.capitalize
+    cohort = STDIN.gets.chomp.capitalize
     cohort = 'Unknown' if cohort.empty?
     puts "Please enter #{name}'s hobby".center(50, '-')
-    hobby = gets.chomp
+    hobby = STDIN.gets.chomp
     puts "Please enter #{name}'s birthplace".center(50, '-')
-    birthplace = gets.chomp.capitalize
+    birthplace = STDIN.gets.chomp.capitalize
     puts "Please enter #{name}'s height in cm".center(50, '-')
-    height = gets.chomp
+    height = STDIN.gets.chomp
     @students << {name: name, cohort: cohort, hobby: hobby, birthplace: birthplace, height: height}
     @students.length == 1 ? (puts "Now we have 1 student") : (puts "Now we have #{@students.count} students".center(50, '-'))
   end
@@ -61,7 +61,7 @@ def print_by_cohort
   puts "Which cohort would you like to print?".center(50, '-')
   puts "To finish, just hit return twice".center(50, '-')
   while true do
-    input = gets.chomp.capitalize
+    input = STDIN.gets.chomp.capitalize
     break if input.empty?
     @students.map do |student|
       if student[:cohort] == input
@@ -82,6 +82,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -93,6 +94,8 @@ def process(selection)
       print_students_list
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit
     else
@@ -101,9 +104,7 @@ def process(selection)
 end
 
 def save_students
-  # open the file for writing
   file = File.open("students.csv", "w")
-  # iterate over the array of students
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
@@ -112,11 +113,33 @@ def save_students
   file.close
 end
 
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
+def try_load_students
+  filename = ARGV.first # first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exists?(filename) # if it exists
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist"
+    exit # quit the program
   end
 end
 
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
+end
+
+try_load_students
 interactive_menu
